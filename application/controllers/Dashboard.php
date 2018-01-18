51dcab157459273dbae4118b87e22c227d56f426
jackeryjam <jackeryjam@gmail.com>
2018-01-18 22:32:21 +0800

    md

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -7,7 +7,7 @@ class Dashboard extends CI_Controller {
 	{
 		$this->load->helper('url');
 		$res = shell_exec("ls /");
-		$data['arr'] = explode("\t" ,$res);
+		$data['arr'] = explode("\n" ,$res);
 		$this->load->view('dashboard', $data);
 	}
 }
