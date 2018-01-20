b2ff5bc315c988b0e472d6c93556e0cbd3a1a7c1
jackeryjam <jackeryjam@gmail.com>
2018-01-20 21:35:30 +0800

    md

--- application/views/dashboard/main.php
+++ application/views/dashboard/main.php
@@ -1,5 +1,6 @@
 <main class="mdl-layout__content">
     <div class="page-content">
+        <?php print_r($systemlist);?>
         <?php foreach ($systemlist as $item): ?>
             <div class="my-mdl-card mdl-card mdl-shadow--2dp">
                 <div class="mdl-card__title">
