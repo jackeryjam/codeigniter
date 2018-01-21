9aa50ab42c44d9ba35c7f148421417286e2b032c
jackeryjam <jackeryjam@gmail.com>
2018-01-21 10:13:06 +0800

    t

--- application/models/Systems.php
+++ application/models/Systems.php
@@ -51,7 +51,7 @@ class Systems extends CI_Model {
             $default = $default.$str."\n";
         }
         $target = '/pub/'.$systemName.'/sourse';
-        preg_match('/pub/\.*?/sourse/', $default);
+        preg_match('/pub/\\.*?/sourse/', $default);
         // $default = preg_replace('/pub/.*?/sourse/',$target, $default);
         echo $default;
     }
