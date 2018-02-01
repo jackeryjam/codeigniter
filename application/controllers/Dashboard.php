6830d9f75ac2e74b8869ca0c3182518244ec7333
jackeryjam <jackeryjam@gmail.com>
2018-02-01 09:24:24 +0800

    md

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -5,7 +5,7 @@ class Dashboard extends CI_Controller {
 	public function __construct()
     {
         parent::__construct();
-        // $this->load->model('Systems');
+        $this->load->model('Systems');
 		$this->load->helper('url');
     }
 
